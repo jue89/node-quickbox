8d2701d5d60df82cc822ca50edeb4eb0772b1dc9
Jue <me@jue.yt>
2021-11-16 23:54:18 +0100

    Fix alignment for geom2 groups

--- alignment.js
+++ alignment.js
@@ -1,27 +1,27 @@
 const {align} = require('@jscad/modeling').transforms;
 
 function TR (point, element) {
-	return align({modes: ['max', 'max'], relativeTo: point}, element);
+	return align({modes: ['max', 'max'], grouped: true, relativeTo: point}, element);
 }
 
 function BR (point, element) {
-	return align({modes: ['max', 'min'], relativeTo: point}, element);
+	return align({modes: ['max', 'min'], grouped: true, relativeTo: point}, element);
 }
 
 function BL (point, element) {
-	return align({modes: ['min', 'min'], relativeTo: point}, element);
+	return align({modes: ['min', 'min'], grouped: true, relativeTo: point}, element);
 }
 
 function TL (point, element) {
-	return align({modes: ['min', 'max'], relativeTo: point}, element);
+	return align({modes: ['min', 'max'], grouped: true, relativeTo: point}, element);
 }
 
 function CC (point, element) {
-	return align({modes: ['center', 'center'], relativeTo: point}, element);
+	return align({modes: ['center', 'center'], grouped: true, relativeTo: point}, element);
 }
 
 function BC (point, element) {
-	return align({modes: ['center', 'min'], relativeTo: point}, element);
+	return align({modes: ['center', 'min'], grouped: true, relativeTo: point}, element);
 }
 
 module.exports = {TR, BR, BL, TL, CC, BC};
